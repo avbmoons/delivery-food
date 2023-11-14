f0844d404683331d4a44f636ff8500d0ecf11593
Elena-admin <avb.moons@gmail.com>
2023-11-14 23:36:15 +0300

    add some scripts

--- scripts/test.js
+++
@@ -1,5 +0,0 @@
-var link = document.querySelector('link[rel=import]');
-
-var content = link.import.querySelector('#test');
-//var content = link.import.getElementById("test");
-document.body.appendChild(content.cloneNode(true));
