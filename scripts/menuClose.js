5ec7a1a48ce777e35c8095784291a90d8f7eea5e
Elena-admin <avb.moons@gmail.com>
2024-02-04 23:07:46 +0300

    add media mobile

---
+++ scripts/menuClose.js
@@ -0,0 +1,9 @@
+const menuMini = document.getElementsByClassName("menu-mini-box")[0];
+
+function menuOpen() {
+  menuMini.style.display = "flex";
+}
+
+function menuClose() {
+  menuMini.style.display = "none";
+}
